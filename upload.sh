313a40fb6aacd7d178c0ad689d2a7d587d433f27
Christian Liesch <ia97lies@users.sourceforge.net>
2011-02-07 06:49:43 +0100

    No CVS anymore

--- upload.sh
+++ upload.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-CVS_TAG=$1
-
-# check if ChangeLog ist
-VERSION=`echo $CVS_TAG | awk 'BEGIN { FS="_" } { printf("%d.%d.%d", $3, $4, $5) }'`
+VERSION=$1
 
 scp ChangeLog ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt/httest-${VERSION}/.
 scp NEWS ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt/httest-${VERSION}/.
