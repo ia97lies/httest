4c35b5924617ff58ff96aa1476159d69f92c760c
Christian Liesch <ia97lies@users.sourceforge.net>
2011-08-03 22:24:50 +0200

    add unit test to generate makefile script

--- test/generate_makefile_am.sh
+++ test/generate_makefile_am.sh
@@ -4,4 +4,8 @@ printf "EXTRA_DIST= "
 for e in `ls -1 *.htt *.htb *.hte *.txt *.sh *.pem *.ntlm`; do echo \\; printf "\t$e "; done
 echo
 echo
-echo TESTS = run_htt_test.sh run_htt_shell_test.sh run_hte_test.sh run_ntlm_test.sh run_coredump_test.sh
+echo "test_store_SOURCES=test_store.c \$(top_srcdir)/src/store.c"
+echo "AM_CFLAGS=-I\$(top_srcdir)/src"
+echo "check_PROGRAMS=test_store"
+echo "TESTS = test_store run_htt_test.sh run_htt_shell_test.sh run_hte_test.sh run_ntlm_test.sh run_coredump_test.sh"
+
