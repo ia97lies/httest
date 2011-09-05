2bd059a45eea526852d621b0f5cbefb2a3ea9a77
Christian Liesch <ia97lies@users.sourceforge.net>
2011-09-05 17:15:24 +0200

    copy all c and h files to win/src

--- generate_win_src.sh
+++ generate_win_src.sh
@@ -2,7 +2,7 @@
 
 rm -rf win/src/*
 mkdir -p win/src
-cp -r src/conf.[c,h] src/defines.h src/file.[c,h] src/htproxy.c src/htremote.c src/htntlm.c src/httest.c src/regex.[c,h] src/socket.[c,h] src/ssl.[c,h] src/util.[c,h] src/worker.[c,h]  win/src/ 
+cp -r src/*.[c,h] win/src/ 
 cp -r tools/hturlext.c tools/htx2b.c win/src/
 grep VERSION config/config.h > win/src/config.h
 cd win
