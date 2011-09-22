e8f31ed7b8ffc33de06d3075001938780f11028b
Christian Liesch <ia97lies@users.sourceforge.net>
2011-09-22 22:50:37 +0200

    new tests

--- test/generate_makefile_am.sh
+++ test/generate_makefile_am.sh
@@ -7,5 +7,5 @@ echo
 echo "test_store_SOURCES=test_store.c \$(top_srcdir)/src/store.c"
 echo "AM_CFLAGS=-I\$(top_srcdir)/src"
 echo "check_PROGRAMS=test_store"
-echo "TESTS = test_store run_htt_test.sh run_htt_shell_test.sh run_hte_test.sh run_ntlm_test.sh run_coredump_test.sh"
+echo "TESTS = test_store run_htt_test.sh run_htt_shell_test.sh run_hte_test.sh run_visual_test.sh run_ntlm_test.sh run_coredump_test.sh"
 
