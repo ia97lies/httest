ab7d294bb066cb1d78aa1b97239669a675dfabc2
Christian Liesch <ia97lies@users.sourceforge.net>
2012-01-26 17:38:42 +0100

    remove ebuild from sourceforge

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ VERSION=$1
 scp ChangeLog ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
 scp NEWS ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
 scp httest-${VERSION}.tar.gz ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
-scp httest-${VERSION}.ebuild ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
+#scp httest-${VERSION}.ebuild ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
 #scp httest-${VERSION}.exe ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
 scp doc/users-guide/users-guide.pdf ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
 
