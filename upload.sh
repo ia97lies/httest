2b1392c72c411963b1605b0dfa7052f7977f72bf
Christian Liesch <ia97lies@users.sourceforge.net>
2012-02-07 22:32:20 +0100

    Generate/upload snapshot

--- upload.sh
+++ upload.sh
@@ -2,10 +2,13 @@
 
 VERSION=$1
 
-scp ChangeLog ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
-scp NEWS ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
-scp httest-${VERSION}.tar.gz ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
-#scp httest-${VERSION}.ebuild ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
-#scp httest-${VERSION}.exe ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
-scp doc/users-guide/users-guide.pdf ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/httest-${VERSION}/.
+if [ ! $VERSION=snapshots ]; then
+  HTTEST=httest-${VERSION}
+else
+  HTTEST=snapshot
+fi
+scp ChangeLog ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/${HTTEST}/.
+scp NEWS ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/${HTTEST}/.
+scp httest-${VERSION}.tar.gz ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/${HTTEST}/.
+scp doc/users-guide/users-guide.pdf ia97lies,htt@frs.sourceforge.net:/home/pfs/project/h/ht/htt/htt2.1/${HTTEST}/.
 
