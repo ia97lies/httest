74e37f49bf0d3a0b23fbc2f322d7c3858f35f377
Christian Liesch <ia97lies@users.sourceforge.net>
2012-03-26 21:35:02 +0200

    js module start

---
+++ src/js_module.c
@@ -0,0 +1,252 @@
+/**
+ * Copyright 2010 Christian Liesch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file
+ *
+ * @Author christian liesch <liesch@gmx.ch>
+ *
+ * Implementation of the HTTP Test Tool js module 
+ */
+
+/************************************************************************
+ * Includes
+ ***********************************************************************/
+#include "module.h"
+#include <jsapi.h>
+
+/************************************************************************
+ * Definitions 
+ ***********************************************************************/
+const char * js_module = "js_module";
+
+typedef struct js_gconf_s {
+  int do_read_line;
+  apr_size_t length;
+} js_gconf_t;
+
+typedef struct js_wconf_s {
+  int starting_line_nr; 
+  apr_table_t *params;
+  apr_table_t *retvars;
+  char *buffer;
+} js_wconf_t;
+
+/************************************************************************
+ * Globals 
+ ***********************************************************************/
+  
+/* The class of the global object. */  
+static JSClass global_class = {  
+    "global", JSCLASS_GLOBAL_FLAGS,  
+    JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_StrictPropertyStub,  
+    JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, JS_FinalizeStub,  
+    JSCLASS_NO_OPTIONAL_MEMBERS  
+};  
+
+/************************************************************************
+ * Local 
+ ***********************************************************************/
+
+/**
+ * Get js config from global 
+ *
+ * @param global IN 
+ * @return js config
+ */
+static js_gconf_t *js_get_global_config(global_t *global) {
+  js_gconf_t *config = module_get_config(global->config, js_module);
+  if (config == NULL) {
+    config = apr_pcalloc(global->pool, sizeof(*config));
+    module_set_config(global->config, apr_pstrdup(global->pool, js_module), config);
+  }
+  return config;
+}
+
+/**
+ * Get js config from worker
+ *
+ * @param worker IN worker
+ * @return js config
+ */
+static js_wconf_t *js_get_worker_config(worker_t *worker) {
+  js_wconf_t *config = module_get_config(worker->config, js_module);
+  if (config == NULL) {
+    config = apr_pcalloc(worker->pbody, sizeof(*config));
+    config->params = apr_table_make(worker->pbody, 5);
+    config->retvars = apr_table_make(worker->pbody, 5);
+    module_set_config(worker->config, apr_pstrdup(worker->pbody, js_module), config);
+  }
+  return config;
+}
+  
+/* The error reporter callback. */  
+static void js_log_error(JSContext *cx, const char *message, JSErrorReport *report) {  
+  fprintf(stderr, "%s:%u:%s\n",  
+          report->filename ? report->filename : "<no filename=\"filename\">",  
+          (unsigned int) report->lineno,  message);  
+}  
+
+/**
+ * Get variable names for in/out for mapping it to/from js 
+ * @param worker IN callee
+ * @param line IN command line
+ */
+static void js_set_variable_names(worker_t *worker, char *line) {
+  char *token;
+  char *last;
+
+  int input = 1;
+  js_wconf_t *config = js_get_worker_config(worker);
+  char *data = apr_pstrdup(worker->pbody, line);
+ 
+  /* Get params and returns variable names for later mapping from/to js */
+  token = apr_strtok(data, " ", &last);
+  while (token) {
+    if (strcmp(token, ":") == 0) {
+      /* : is separator between input and output vars */
+      input = 0;
+    }
+    else {
+      if (input) {
+        apr_table_setn(config->params, token, token);
+      }
+      else {
+        apr_table_setn(config->retvars, token, token);
+      }
+    }
+    token = apr_strtok(NULL, " ", &last);
+  }
+}
+
+/**
+ * Simple js interpreter for js block
+ * @param worker IN callee
+ * @param parent IN caller
+ * @param ptmp IN temp pool for this function
+ * @return apr status
+ */
+static apr_status_t block_js_interpreter(worker_t *worker, worker_t *parent, 
+                                         apr_pool_t *ptmp) {
+  js_wconf_t *wconf = js_get_worker_config(worker);
+
+  return APR_SUCCESS;
+}
+
+/************************************************************************
+ * Hooks 
+ ***********************************************************************/
+
+/**
+ * Do load a js block
+ * @param global IN
+ * @param line INOUT line 
+ * @return APR_SUCCESS
+ */
+static apr_status_t js_block_start(global_t *global, char **line) {
+  apr_status_t status;
+  if (strncmp(*line, ":JS ", 4) == 0) {
+    *line += 4;
+    if ((status = worker_new(&global->worker, "", "", global, 
+                             block_js_interpreter)) 
+        != APR_SUCCESS) {
+      return status;
+    }
+    else {
+      js_wconf_t *wconf = js_get_worker_config(global->worker);
+      js_gconf_t *gconf = js_get_global_config(global);
+      gconf->do_read_line = 1;
+      wconf->starting_line_nr = global->line_nr;
+      js_set_variable_names(global->worker, *line);
+      return APR_SUCCESS;
+    }
+  }
+  return APR_ENOTIMPL;
+}
+
+/**
+ * Read line of block 
+ * @param global IN
+ * @param line INOUT line 
+ * @return APR_SUCCESS
+ */
+static apr_status_t js_read_line(global_t *global, char **line) {
+  js_gconf_t *gconf = js_get_global_config(global);
+  if (gconf->do_read_line) {
+    if (*line[0] == 0) {
+      *line = apr_pstrdup(global->pool, " ");
+    }
+    gconf->length += strlen((*line)) + 1;
+  }
+  return APR_SUCCESS;
+}
+
+/**
+ * Do load a js block
+ * @param global IN
+ * @param line INOUT line 
+ * @return APR_SUCCESS
+ */
+static apr_status_t js_block_end(global_t *global) {
+  js_gconf_t *gconf = js_get_global_config(global);
+  js_wconf_t *wconf = js_get_worker_config(global->worker);
+  gconf->do_read_line = 0;
+  if (gconf->length) {
+    int i;
+    apr_table_entry_t *e;
+    char *buf;
+
+    wconf->buffer = apr_pcalloc(global->worker->pbody, gconf->length);
+    buf = wconf->buffer;
+    e = (apr_table_entry_t *) apr_table_elts(global->worker->lines)->elts;
+    for (i = 0; i < apr_table_elts(global->worker->lines)->nelts; i++) {
+      strcpy(buf, e[i].val);
+      buf += strlen(e[i].val);
+      *buf = '\n';
+      ++buf;
+    }
+
+  fprintf(stderr, "\nYYY\n%s\nYYY\n", wconf->buffer);
+  fflush(stderr);
+  }
+  return APR_SUCCESS;
+}
+
+/************************************************************************
+ * Commands 
+ ***********************************************************************/
+static apr_status_t block_JS_DUMMY(worker_t *worker, worker_t *parent, apr_pool_t *ptmp) {
+  return APR_SUCCESS;
+}
+
+/************************************************************************
+ * Module
+ ***********************************************************************/
+apr_status_t js_module_init(global_t *global) {
+  apr_status_t status;
+  if ((status = module_command_new(global, "JS", "_DUMMY",
+	                           "<foo>",
+	                           "Bla bla bla.",
+	                           block_JS_DUMMY)) != APR_SUCCESS) {
+    return status;
+  }
+  htt_hook_block_start(js_block_start, NULL, NULL, 0);
+  htt_hook_read_line(js_read_line, NULL, NULL, 0);
+  htt_hook_block_end(js_block_end, NULL, NULL, 0);
+
+  return APR_SUCCESS;
+}
+
