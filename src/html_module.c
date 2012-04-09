a4005c0f22c04bfd08b85ef548cd4ee9dcfccf5a
Christian Liesch <ia97lies@users.sourceforge.net>
2012-04-09 17:20:24 +0200

    start with an xpath query command

--- src/html_module.c
+++ src/html_module.c
@@ -27,6 +27,7 @@
  ***********************************************************************/
 #include "module.h"
 #include "libxml/HTMLparser.h"
+#include "libxml/xpath.h"
 
 /************************************************************************
  * Definitions 
@@ -34,7 +35,7 @@
 typedef struct html_wconf_s {
   htmlParserCtxtPtr parser_ctx;
   htmlDocPtr doc;
-  xmlNodePtr root_node;
+  xmlXPathContextPtr xpath;
 } html_wconf_t;
 
 /************************************************************************
@@ -64,11 +65,16 @@ static html_wconf_t *html_get_worker_config(worker_t *worker) {
 /************************************************************************
  * Commands 
  ***********************************************************************/
+/**
+ * Parse HTML block
+ * @param worker IN callee
+ * @param parent IN caller
+ * @param ptmp IN temporary pool
+ * @return apr status
+ */
 static apr_status_t block_HTML_PARSE(worker_t *worker, worker_t *parent, apr_pool_t *ptmp) {
   const char *param;
-  const xmlChar *html;
-  xmlNodePtr cur;
-  xmlNodePtr sib;
+  const char *html;
 
   html_wconf_t *wconf = html_get_worker_config(worker);
 
@@ -79,27 +85,47 @@ static apr_status_t block_HTML_PARSE(worker_t *worker, worker_t *parent, apr_poo
   }
   html = worker_get_value_from_param(worker, param, ptmp);
 
-  wconf->doc = htmlCtxtReadDoc(wconf->parser_ctx, html, NULL, NULL, 0);
+  wconf->doc = htmlCtxtReadDoc(wconf->parser_ctx, (xmlChar *)html, NULL, NULL, 0);
   if (!wconf->doc) {
     worker_log_error(worker, "Could not parse HTML");
     return APR_EINVAL;
   }
-  wconf->root_node = xmlDocGetRootElement(wconf->doc);
+  wconf->xpath = xmlXPathNewContext(wconf->doc);
   
-#if 0
-  cur = wconf->root_node;
-  while (cur) {
-    sib = cur;
-    while (sib) {
-      fprintf(stderr, "\nXXX: %s:%s\n", sib->name, sib->content);
-      sib = sib->next;
-    }
-    cur = cur->children;
-  }
-#endif
   return APR_SUCCESS;
 }
 
+/**
+ * Xpath query on a parsed HTML block
+ * @param worker IN callee
+ * @param parent IN caller
+ * @param ptmp IN temporary pool
+ * @return apr status
+ */
+static apr_status_t block_HTML_XPATH(worker_t *worker, worker_t *parent, apr_pool_t *ptmp) {
+  const char *param;
+  xmlXPathObjectPtr objs; 
+
+  html_wconf_t *wconf = html_get_worker_config(worker);
+
+  param = store_get(worker->params, "1");
+  if (!param) {
+    worker_log_error(worker, "Need a xpath query");
+    return APR_EINVAL;
+  }
+
+  if (!wconf->xpath) {
+    worker_log_error(worker, "Do _HTML:PARSE first");
+    return APR_EGENERAL;
+  }
+  
+  if ((objs = xmlXPathEval(param, wconf->xpath)) == NULL) {
+    worker_log_error(worker, "Xpath error");
+    return APR_EGENERAL;
+  }
+
+  return APR_SUCCESS;
+}
 /************************************************************************
  * Module
  ***********************************************************************/
@@ -111,6 +137,13 @@ apr_status_t html_module_init(global_t *global) {
 	                           block_HTML_PARSE)) != APR_SUCCESS) {
     return status;
   }
+  if ((status = module_command_new(global, "HTML", "_XPATH",
+	                           "<query>",
+	                           "Return requested object",
+	                           block_HTML_XPATH)) != APR_SUCCESS) {
+    return status;
+  }
+
   return APR_SUCCESS;
 }
 
