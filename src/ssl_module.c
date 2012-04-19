977d700aae7cc0b49e45384f71f3a64871a96233
Christian Liesch <ia97lies@users.sourceforge.net>
2012-04-20 00:19:02 +0200

    Debug infos for SSL Handshake

--- src/ssl_module.c
+++ src/ssl_module.c
@@ -200,6 +200,262 @@ static apr_status_t worker_ssl_ctx_p12(worker_t * worker, const char *infile,
 }
 
 /**
+ * Message call back for debugging
+ */
+static void msg_cb(int write_dir, int version, int content_type, 
+                   const void *buf, size_t len, SSL *ssl, void *arg) {
+  worker_t *worker = arg;
+  const char *prefix;
+  const char *version_string;
+  const char *content_type_string = "";
+  const char *details1 = "";
+  const char *details2= "";
+
+  prefix = write_dir ? ">" : "<";
+
+  switch (version)
+  {
+    case SSL2_VERSION:
+      version_string = "SSL 2.0";
+      break;
+    case SSL3_VERSION:
+      version_string = "SSL 3.0";
+      break;
+    case TLS1_VERSION:
+      version_string = "TLS 1.0";
+      break;
+    default:
+      version_string = "???";
+    case DTLS1_VERSION:
+      version_string = "DTLS 1.0";
+      break;
+    case DTLS1_BAD_VER:
+      version_string = "DTLS 1.0 (bad)";
+      break;
+  }
+
+  if (version == SSL2_VERSION)
+  {
+    details1 = "???";
+
+    if (len > 0)
+    {
+      switch (((const unsigned char*)buf)[0])
+      {
+        case 0:
+          details1 = ", ERROR:";
+          details2 = " ???";
+          if (len >= 3)
+          {
+            unsigned err = (((const unsigned char*)buf)[1]<<8) + ((const unsigned char*)buf)[2];
+
+            switch (err)
+            {
+              case 0x0001:
+                details2 = " NO-CIPHER-ERROR";
+                break;
+              case 0x0002:
+                details2 = " NO-CERTIFICATE-ERROR";
+                break;
+              case 0x0004:
+                details2 = " BAD-CERTIFICATE-ERROR";
+                break;
+              case 0x0006:
+                details2 = " UNSUPPORTED-CERTIFICATE-TYPE-ERROR";
+                break;
+            }
+          }
+
+          break;
+        case 1:
+          details1 = ", CLIENT-HELLO";
+          break;
+        case 2:
+          details1 = ", CLIENT-MASTER-KEY";
+          break;
+        case 3:
+          details1 = ", CLIENT-FINISHED";
+          break;
+        case 4:
+          details1 = ", SERVER-HELLO";
+          break;
+        case 5:
+          details1 = ", SERVER-VERIFY";
+          break;
+        case 6:
+          details1 = ", SERVER-FINISHED";
+          break;
+        case 7:
+          details1 = ", REQUEST-CERTIFICATE";
+          break;
+        case 8:
+          details1 = ", CLIENT-CERTIFICATE";
+          break;
+      }
+    }
+  }
+
+  if (version == SSL3_VERSION ||
+      version == TLS1_VERSION ||
+      version == DTLS1_VERSION ||
+      version == DTLS1_BAD_VER)
+  {
+    switch (content_type)
+    {
+      case 20:
+        content_type_string = "ChangeCipherSpec";
+        break;
+      case 21:
+        content_type_string = "Alert";
+        break;
+      case 22:
+        content_type_string = "Handshake";
+        break;
+    }
+
+    if (content_type == 21) /* Alert */
+    {
+      details1 = ", ???";
+
+      if (len == 2)
+      {
+        switch (((const unsigned char*)buf)[0])
+        {
+          case 1:
+            details1 = ", warning";
+            break;
+          case 2:
+            details1 = ", fatal";
+            break;
+        }
+
+        details2 = " ???";
+        switch (((const unsigned char*)buf)[1])
+        {
+          case 0:
+            details2 = " close_notify";
+            break;
+          case 10:
+            details2 = " unexpected_message";
+            break;
+          case 20:
+            details2 = " bad_record_mac";
+            break;
+          case 21:
+            details2 = " decryption_failed";
+            break;
+          case 22:
+            details2 = " record_overflow";
+            break;
+          case 30:
+            details2 = " decompression_failure";
+            break;
+          case 40:
+            details2 = " handshake_failure";
+            break;
+          case 42:
+            details2 = " bad_certificate";
+            break;
+          case 43:
+            details2 = " unsupported_certificate";
+            break;
+          case 44:
+            details2 = " certificate_revoked";
+            break;
+          case 45:
+            details2 = " certificate_expired";
+            break;
+          case 46:
+            details2 = " certificate_unknown";
+            break;
+          case 47:
+            details2 = " illegal_parameter";
+            break;
+          case 48:
+            details2 = " unknown_ca";
+            break;
+          case 49:
+            details2 = " access_denied";
+            break;
+          case 50:
+            details2 = " decode_error";
+            break;
+          case 51:
+            details2 = " decrypt_error";
+            break;
+          case 60:
+            details2 = " export_restriction";
+            break;
+          case 70:
+            details2 = " protocol_version";
+            break;
+          case 71:
+            details2 = " insufficient_security";
+            break;
+          case 80:
+            details2 = " internal_error";
+            break;
+          case 90:
+            details2 = " user_canceled";
+            break;
+          case 100:
+            details2 = " no_renegotiation";
+            break;
+        }
+      }
+    }
+
+    if (content_type == 22) /* Handshake */
+    {
+      details1 = "???";
+
+      if (len > 0)
+      {
+        switch (((const unsigned char*)buf)[0])
+        {
+          case 0:
+            details1 = ", HelloRequest";
+            break;
+          case 1:
+            details1 = ", ClientHello";
+            break;
+          case 2:
+            details1 = ", ServerHello";
+            break;
+          case 11:
+            details1 = ", Certificate";
+            break;
+          case 12:
+            details1 = ", ServerKeyExchange";
+            break;
+          case 13:
+            details1 = ", CertificateRequest";
+            break;
+          case 14:
+            details1 = ", ServerHelloDone";
+            break;
+          case 15:
+            details1 = ", CertificateVerify";
+            break;
+          case 3:
+            details1 = ", HelloVerifyRequest";
+            break;
+          case 16:
+            details1 = ", ClientKeyExchange";
+            break;
+          case 20:
+            details1 = ", Finished";
+            break;
+        }
+      }
+    }
+  }
+
+  worker_log(worker, LOG_INFO, "%s%s: %s%s%s", prefix, 
+             version_string, content_type_string, details1, details2);
+}
+
+/**
  * Get server ctx with loaded cert and key file
  *
  * @param worker IN thread object data
@@ -383,6 +639,8 @@ static apr_status_t worker_ssl_accept(worker_t * worker) {
 	status = APR_ECONNREFUSED;
       }
       SSL_set_ssl_method(sconfig->ssl, config->meth);
+      SSL_set_msg_callback(sconfig->ssl, msg_cb);
+      SSL_set_msg_callback_arg(sconfig->ssl, worker);
       ssl_rand_seed();
       apr_os_sock_get(&fd, worker->socket->socket);
       bio = BIO_new_socket(fd, BIO_NOCLOSE);
@@ -561,10 +819,12 @@ static apr_status_t block_SSL_CONNECT(worker_t * worker, worker_t *parent, apr_p
       }
 
       if ((sconfig->ssl = SSL_new(config->ssl_ctx)) == NULL) {
-				worker_log(worker, LOG_ERR, "SSL_new failed.");
-				return APR_ECONNREFUSED;
+        worker_log(worker, LOG_ERR, "SSL_new failed.");
+        return APR_ECONNREFUSED;
       }
       SSL_set_ssl_method(sconfig->ssl, config->meth);
+      SSL_set_msg_callback(sconfig->ssl, msg_cb);
+      SSL_set_msg_callback_arg(sconfig->ssl, worker);
       ssl_rand_seed();
       apr_os_sock_get(&fd, worker->socket->socket);
       bio = BIO_new_socket(fd, BIO_NOCLOSE);
@@ -1241,6 +1501,8 @@ static apr_status_t ssl_hook_connect(worker_t *worker) {
       return APR_EGENERAL;
     }
     SSL_set_ssl_method(sconfig->ssl, config->meth);
+    SSL_set_msg_callback(sconfig->ssl, msg_cb);
+    SSL_set_msg_callback_arg(sconfig->ssl, worker);
     ssl_rand_seed();
     apr_os_sock_get(&fd, worker->socket->socket);
     bio = BIO_new_socket(fd, BIO_NOCLOSE);
