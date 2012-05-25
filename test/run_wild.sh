4b1522c811f16d133c60b2fc938814b2265e5618
Christian Liesch <ia97lies@users.sourceforge.net>
2012-05-25 07:00:25 +0200

    use run_lib.sh for run_wild

--- test/run_wild.sh
+++ test/run_wild.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-HTT_ERRORS=0
+if [ -z $srcdir ]; then
+  srcdir=.
+fi
 
-for E in `ls ${@}`; do
-  CORES_PRE=`ls core* 2>/dev/null | wc -l` 
-  ./run.sh -e $E
-  if [ $? -ne 0 ]; then
-    HTT_ERRORS=`expr $HTT_ERRORS + 1`
-  fi
-  CORES_POST=`ls core* 2>/dev/null | wc -l` 
-  if [ $CORES_POST -gt $CORES_PRE ]; then
-    echo Coredump detected!
-  fi
-done
+. $srcdir/run_lib.sh
 
-exit $HTT_ERRORS
+function run_single {
+  E=$1
+  OUT=$2
+
+  ./run.sh $E >> $OUT 2>> $OUT
+}
+
+echo normal test execution
+LIST=`ls ${@}`
+COUNT=`ls ${@} | wc -l`
+run_all "$LIST" $COUNT
+
