3e819783935c128889c85497fd637879522bade9
jexler <jex@jexler.net>
2012-06-24 22:17:00 +0200

    htcolor.pl perl script for coloring httest output on unix shell

--- tools/htcolor.pl
+++ tools/htcolor.pl
@@ -3,29 +3,29 @@
 # colors httest output, just pipe into this script and see...
 #
 while (<STDIN>) {
-	my $line = $_;
-	chomp($line);
-	if ($line =~ m/^>/) {
-	  # CLIENT: dark blue
-	  print "\033[1;34m$line\033[0m\n";
+    my $line = $_;
+    chomp($line);
+    if ($line =~ m/^>/) {
+      # CLIENT: dark blue
+      print "\033[1;34m$line\033[0m\n";
     } elsif ($line =~ m/^</) {
       # CLIENT: light blue
       print "\033[0;34m$line\033[0m\n";
-	} elsif ($line =~ m/^ {24,24}>/) {
-	  # SERVER: dark green
-	  print "\033[1;32m$line\033[0m\n";
+    } elsif ($line =~ m/^ {24,24}>/) {
+      # SERVER: dark green
+      print "\033[1;32m$line\033[0m\n";
     } elsif ($line =~ m/^ {24,24}</) {
       #  SERVER: light green
       print "\033[0;32m$line\033[0m\n";
-	} elsif ($line =~ m/^ {48,48}>/) {
-	  # SERVER 2: dark purple
-	  print "\033[1;35m$line\033[0m\n";
+    } elsif ($line =~ m/^ {48,48}>/) {
+      # SERVER 2: dark purple
+      print "\033[1;35m$line\033[0m\n";
     } elsif ($line =~ m/^ {48,48}</) {
       # SERVER 2: light purple
       print "\033[0;35m$line\033[0m\n";
-	} elsif ($line =~ m/^ +>/) {
-	  # SERVER X: dark brown
-	  print "\033[1;33m$line\033[0m\n";
+    } elsif ($line =~ m/^ +>/) {
+      # SERVER X: dark brown
+      print "\033[1;33m$line\033[0m\n";
     } elsif ($line =~ m/^ +</) {
       # SERVER X: light brown
       print "\033[0;33m$line\033[0m\n";
