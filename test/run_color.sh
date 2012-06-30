db522efce9af0b47196e11ceaa98e3ae1c2970f5
jexler <jex@jexler.net>
2012-06-30 07:01:19 +0200

    htcolor refinements

--- test/run_color.sh
+++ test/run_color.sh
@@ -5,4 +5,7 @@ export TOP
 
 HTTEST=../src/httest
 HTCOLOR=../tools/htcolor
-{ $HTTEST $@ 2>&3 | $HTCOLOR; } 3>&1 1>&2 | $HTCOLOR -e
+# set if you need the httest exit code
+#set -o pipefail
+{ { $HTTEST $@ 1>&3 2>&4; } 4>&1 | $HTCOLOR -e; } 3>&1 1>&2 | $HTCOLOR
+#exit $?
