ce016f2b8d0f0f279448927ef30ada7cba96f7ce
Christian Liesch <ia97lies@users.sourceforge.net>
2012-07-22 22:29:28 +0200

    start with httest 3.0

---
+++ src/htt_log.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2006 Christian Liesch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file
+ *
+ * @Author christian liesch <liesch@gmx.ch>
+ *
+ * Interface of the htt log.
+ */
+
+#ifndef HTT_LOG_H
+#define HTT_LOG_H
+
+#define LOG_NONE 0
+#define LOG_ERR 1
+#define LOG_WARN 2
+#define LOG_INFO 3
+#define LOG_CMD 4
+#define LOG_ALL_CMD 5
+#define LOG_DEBUG 6
+
+#endif
