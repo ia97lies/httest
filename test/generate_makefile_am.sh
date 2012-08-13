658d8c274a3633654a348e1ee0a81be670504c4d
Christian Liesch <ia97lies@users.sourceforge.net>
2012-08-13 17:33:37 +0200

    Stack unit test skeleton

--- test/generate_makefile_am.sh
+++ test/generate_makefile_am.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-printf "EXTRA_DIST= "
-for e in `ls -1 *.htt *.htb *.hte *.txt *.sh *.pem *.ntlm *.visual`; do echo \\; printf "\t$e "; done
-echo
-echo
-echo "SUBDIRS= sub"
-echo "test_store_SOURCES=test_store.c \$(top_srcdir)/src/store.c"
+echo "test_htt_stack_SOURCES=test_htt_stack.c \$(top_srcdir)/src/store.c"
 echo "AM_CFLAGS=-I\$(top_srcdir)/src"
-echo "check_PROGRAMS=test_store"
-echo "TESTS = test_store test_run_all.sh test_run_errors.sh test_run_visual.sh test_run_ntlm.sh test_check_coredumps.sh"
+echo "check_PROGRAMS=test_htt_stack"
+echo "TESTS = test_htt_stack"
 
