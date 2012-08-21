88631e724d5c6c76d25e1c56165dd579ab67dacd
Christian Liesch <ia97lies@users.sourceforge.net>
2012-08-21 16:56:47 +0200

    Use a base object for all others

---
+++ src/htt_object.h
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2012 Christian Liesch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file
+ *
+ * @Author christian liesch <liesch@gmx.ch>
+ *
+ * Interface of the HTTP Test Tool object.
+ */
+
+#ifndef HTT_OBJECT_H
+#define HTT_OBJECT_H
+
+#include <apr_pools.h>
+
+typedef void(*htt_destructor_f)(void *e); 
+typedef void*(*htt_clone_f)(void *e, apr_pool_t *pool); 
+typedef struct htt_object_s {
+  int type;
+  apr_pool_t *pool;
+  htt_destructor_f destructor;
+  htt_clone_f clone;
+} htt_object_t;
+
+#endif
