33ecd8a9b25a7dbbb946b5553afdfebe4de222f9
jexler <jex@jexler.net>
2013-02-25 11:32:25 +0100

    -a

--- packages/shrinkwrap/source/all.sh
+++ packages/shrinkwrap/source/all.sh
@@ -536,11 +536,25 @@ function do_unix_build_JS {
 }
 
 #
-# unix: build libmlx2
+# unix: build libxml2
 #
 function unix_build_XML2 {
   cd "$TARGET/$UNIX_XML2_NAME-$UNIX_XML2_VER"
   ./configure
+  if [ "$OS" = "mac" -o "$OS" = "solaris" ]; then
+    mv threads.c threads.c.orig
+    cat threads.c.orig | awk '
+      /static pthread_once_t once_control = PTHREAD_ONCE_INIT;/ {
+        print "static pthread_once_t once_control = PTHREAD_ONCE_INIT;"
+        print "static pthread_once_t once_control_init = PTHREAD_ONCE_INIT;"
+        next
+      }
+      /once_control = PTHREAD_ONCE_INIT;/ {
+        print "once_control = once_control_init;"
+        next
+      }
+      { print $0 }' > threads.c
+  fi
   make
   create_custom_config "xml2" $UNIX_XML2_VER \
     "-I\${DIR}/include" "-L\${DIR} -lxml2"
