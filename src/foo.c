7424d80defe82eb191ca4110f315a266168d0bee
Christian Liesch <ia97lies@users.sourceforge.net>
2013-06-14 12:56:51 +0200

    Cheap test for transport dso

--- src/foo.c
+++ src/foo.c
@@ -2,38 +2,31 @@
 #include "dso.h"
 #include "stdio.h"
 
+char *gbuf = NULL;
+
 static void* foo_custom_handle() {
-  fprintf(stdout, "Custom handle\n");
-  fflush(stdout);
-  return NULL;
+  if (!gbuf) {
+    gbuf = malloc(10000);
+  }
+  return gbuf;
 }
 
 static apr_status_t foo_configure(void *custom, const char *buf) {
-  fprintf(stdout, "configure \"%s\"\n", buf);
-  fflush(stdout);
   return APR_SUCCESS;
 }
 
 static apr_status_t foo_read(void *custom, const char *buf, apr_size_t *len) {
-  fprintf(stdout, "read");
-  fflush(stdout);
+  const char *str = "HTTP/1.1 200 OK\r\n\r\n";
+  *len = strlen(str);
+  memcpy(buf, str, *len);
   return APR_SUCCESS;
 }
 
 static apr_status_t foo_write(void *custom, const char *buf, apr_size_t len) {
-  fprintf(stdout, "write");
-  fflush(stdout);
   return APR_SUCCESS;
 }
 
 transport_dso_t foo_front = {
-  foo_custom_handle, 
-  foo_configure, 
-  foo_read, 
-  foo_write
-};
-
-transport_dso_t foo_back = {
   foo_custom_handle, 
   foo_configure, 
   foo_read, 
