58d0316eb3111f0220d8627bd8b9dcdccd9e4ff2
Armin Abfalterer <armin.abfalterer@united-security-providers.ch>
2016-12-12 14:42:00 +0100

    log reason in case of RST_STREAM

--- src/h2_module.c
+++ src/h2_module.c
@@ -710,7 +710,9 @@ static int h2_on_frame_recv_callback(nghttp2_session *session,
       worker_log(worker, LOG_INFO, "< WINDOW_UPDATE");
       break;
     case NGHTTP2_RST_STREAM:
-      worker_log(worker, LOG_INFO, "< RST_STREAM");
+      worker_log(
+          worker, LOG_INFO, "< RST_STREAM [reason=%s]",
+          h2_get_name_of(h2_error_code_array, frame->rst_stream.error_code));
       break;
     case NGHTTP2_GOAWAY: {
       char *reason = NULL;
