74d0ad551ef8abe2347e43644aa160df96576e0d
Christian Liesch <christian.liesch@adnovum.ch>
2017-11-16 13:28:47 +0100

    Turned on the distcheck target for the distcheck script and fixed a missing header file

--- distcheck.sh
+++ distcheck.sh
@@ -7,6 +7,6 @@ export LDFLAGS="-L/share/xpository/osp/lua/5.2.2/${ARCH}/dist-bin/lib"
 CONFIG="--enable-lua-module --enable-html-module --enable-xml-module --enable-h2-module --with-apr=/share/xpository/apache/apr/1.5.2/$ARCH/dist-bin/bin --with-apr-util=/share/xpository/apache/apr-util/1.5.4/$ARCH/dist-bin/bin --with-ssl=/share/install/adnssl/3.2.4.2/adnssl/spool/$ARCH-prod --with-pcre=/share/xpository/pcre/pcre/8.39/$ARCH/dist-bin/bin --with-libxml2=/share/xpository/gnome/libxml2/2.9.4/$ARCH/dist-bin/bin --with-nghttp2=/share/xpository/github/nghttp2/1.18.0/${ARCH}/dist-bin"
 CFLAGS="-O0 -g -Wall -Wno-declaration-after-statement -Wno-unused-label --pedantic -Werror -std=c99" ./configure $CONFIG
 make clean all
-#make distcheck DISTCHECK_CONFIGURE_FLAGS="$CONFIG"
+make distcheck DISTCHECK_CONFIGURE_FLAGS="$CONFIG"
 make check
 
